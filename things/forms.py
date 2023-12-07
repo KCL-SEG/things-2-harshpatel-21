e33103b26e4625fd6ee2df443ab9c1e7d6463208
harshpatel-21 <patel.harshraj2004@gmail.com>
2023-12-07 17:14:43 +0000

    feat: added form for thing

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,27 @@
 """Forms of the project."""
 
 # Create your forms here.
+from typing import Any
+from django import forms
+from django.contrib.auth import authenticate
+from django.core.validators import RegexValidator
+from django.core.exceptions import ObjectDoesNotExist
+from things.models import Thing
+
+class ThingForm(forms.ModelForm):
+    
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+        widgets = {
+        'description': forms.Textarea(),
+        'quantity': forms.NumberInput(),
+        }
+    
+    def save(self):
+        """Save the provided password in hashed format."""
+        super().clean()
+        thing = super().save(commit=False)
+        thing.save()
+        return thing
